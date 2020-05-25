a3fd3099ccdd45dad2aad2e82c3125f0a92bcd52
David Kirchmer <david.kmsa@gmail.com>
2020-05-25 10:03:01 -0400

    Refactoring - more logical region naming

--- Mitigate-SpeculationControlSettings.ps1
+++ Mitigate-SpeculationControlSettings.ps1
@@ -36,45 +36,6 @@
    The functionality that best describes this cmdlet
  #>
 
-<#
-
-
-#>
-
-
-# Save the current execution policy so it can be reset
-$SavedExecutionPolicy = Get-ExecutionPolicy
-Set-ExecutionPolicy RemoteSigned -Scope Currentuser
-
-# Backup the registry keys for recovery, if needed.
-$HiveMemoryManagement = Get-ChildItem -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management" -Recurse
-$HiveMemoryManagement
-
-Get-ChildItem -Path HKLM:\ -Recurse
-
-#region Determine if PSGallery is Registered as a Trusted PSRepository
-$PSGalleryTrusted = Get-PSRepository -Name PSGallery
-
-# If PSGallery is not yet registered, Register PSGallery as a Trusted PSRepository
-if ($null -eq $PSGalleryTrusted) {
-    Register-PSRepository -SourceLocation "https://www.powershellgallery.com/api/v2" -InstallationPolicy Trusted
-}
-
-# If PSGallery is registered but Untrusted, Set the Installation Policy to "Trusted"
-if ($PSGalleryTrusted.InstallationPolicy -ne "Trusted"){
-    Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
-}
-#endregion Determine if PSGallery is Registered as a Trusted PSRepository
-
-#region Determine if SpeculationControl Module is installed
-$SpeculationControlModule = Get-InstalledModule -Name SpeculationControl
-
-# If SpeculationControl Module is not installed, find it in PSGallery and Install it
-if ($null -eq $SpeculationControlModule) {
-    Find-Module -Name SpeculationControl | Install-Module 
-}
-#endregion Determine if SpeculationControl Module is installed
-
 
 #region Test for the log file path and create it if not found then start the transcript into the log file
 $LogfilePath = "C:\Logs"
@@ -92,44 +53,73 @@ else {
 }
 #endregion Test for the log file path and create it if not found then start the transcript into the log file
 
-<#
-To query the state of configurable mitigations:
-#>
+
+#region Save the current execution policy so it can be reset
+$SavedExecutionPolicy = Get-ExecutionPolicy
+Set-ExecutionPolicy RemoteSigned -Scope Currentuser
+#endregion Save the current execution policy so it can be reset
+
+#region Backup the registry keys for recovery, if needed.
+REG EXPORT "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management" $RegKeysFile
+#endregion Backup the registry keys for recovery, if needed.
 
 
-# Check if SpeculationControl Module is imported and import it if not found
+#region Determine if PSGallery is Registered as a Trusted PSRepository
+$PSGalleryTrusted = Get-PSRepository -Name PSGallery
+
+# If PSGallery is not yet registered, Register PSGallery as a Trusted PSRepository
+if ($null -eq $PSGalleryTrusted) {
+    Register-PSRepository -SourceLocation "https://www.powershellgallery.com/api/v2" -InstallationPolicy Trusted
+}
+
+# If PSGallery is registered but Untrusted, Set the Installation Policy to "Trusted"
+if ($PSGalleryTrusted.InstallationPolicy -ne "Trusted"){
+    Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
+}
+#endregion Determine if PSGallery is Registered as a Trusted PSRepository
+
+
+#region Determine if SpeculationControl Module is installed
+$SpeculationControlModule = Get-InstalledModule -Name SpeculationControl
+
+# If SpeculationControl Module is not installed, find it in PSGallery and Install it
+if ($null -eq $SpeculationControlModule) {
+    Find-Module -Name SpeculationControl | Install-Module 
+}
+#endregion Determine if SpeculationControl Module is installed
+
+
+#region Check if SpeculationControl Module is imported and import it if not found
 $SpeculatoinControlImported = Get-Module -Name "SpeculationControl"
 if ($null -eq $SpeculatoinControlImported){
     Import-Module SpeculationControl #-Verbose
 }
+#endregion Check if SpeculationControl Module is imported and import it if not found
 
-#Get-SpeculationControlSettings Results
 
-$SpeculationControlResults = Get-SpeculationControlSettings
-$SpeculationControlResults          #Displays to console so Transcript will log results
+#region Get-SpeculationControlSettings Results before mitigation
+$SpeculationControlResultsBefore = Get-SpeculationControlSettings
+$SpeculationControlResultsBefore          #Displays to console so Transcript will log results
+#endregion Get-SpeculationControlSettings Results before mitigation
+
+# * Suggested actions
+ 
+<# 
+* Hardware support for branch target injection mitigation is present: False
+    The required hardware features are not present, and therefore the branch target injection mitigation cannot be enabled.
+#>
+ 
+ 
+<# 
+* Windows OS support for branch target injection mitigation is present: False
+    If it is False, the January 2018 update has not been installed on the system, and the branch target injection
+    mitigation cannot be enabled.
+#>
+
+#region Mitigate "Windows OS support for speculative store bypass disable is enabled system-wide: False" vulnerability
 
 <#
- * Understanding Get-SpeculationControlSettings PowerShell script output
- * https://support.microsoft.com/help/4074629
-
- * Windows client guidance for IT Pros to protect against speculative execution side-channel vulnerabilities
- * https://support.microsoft.com/help/4073119
- * 
- * Windows Server guidance to protect against speculative execution side-channel vulnerabilities
- * https://support.microsoft.com/help/4072698
- * 
-
- * Suggested actions
- * 
- * Hardware support for branch target injection mitigation is present: False
-        The required hardware features are not present, and therefore the branch target injection mitigation cannot be enabled.
-
-* Windows OS support for branch target injection mitigation is present: False
-        If it is False, the January 2018 update has not been installed on the system, and the branch target injection
-        mitigation cannot be enabled.
-
- #region Mitigate "Windows OS support for speculative store bypass disable is enabled system-wide: False" vulnerability
-
+* Windows OS support for Speculative Store Bypass Disable is enabled system-wide: True/False result
     To enable mitigations for Intel® Transactional Synchronization Extensions (Intel® TSX) Transaction Asynchronous
     Abort vulnerability (CVE-2019-11135) and Microarchitectural Data Sampling (CVE-2018-11091, CVE-2018-12126,
     CVE-2018-12127, CVE-2018-12130) along with Spectre (CVE-2017-5753 & CVE-2017-5715) and Meltdown (CVE-2017-5754)
@@ -137,8 +127,6 @@ $SpeculationControlResults          #Displays to console so Transcript will log 
     (CVE-2018-3615, CVE-2018-3620, and CVE-2018-3646) without disabling Hyper-Threading:
  
  * If result contains: "SSBDWindowsSupportEnabledSystemWide : False" apply registry keys to mitigate the vulnerability
- * This maps to the Speculation control settings for CVE-2018-3639 [speculative store bypass]: 
-   Windows OS support for Speculative Store Bypass Disable is enabled system-wide: True/False result
 #>
 if ($SpeculationControlResults.SSBDWindowsSupportEnabledSystemWide -ne "True"){
     reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management" /v FeatureSettingsOverride /t REG_DWORD /d 72 /f
@@ -146,10 +134,17 @@ if ($SpeculationControlResults.SSBDWindowsSupportEnabledSystemWide -ne "True"){
 }
 #endregion Mitigate "Windows OS support for speculative store bypass disable is enabled system-wide: False" vulnerability
 
-$SpeculationControlResults = Get-SpeculationControlSettings
-$SpeculationControlResults
 
-# Reset the execution policy to the original state
+#region Get-SpeculationControlSettings Results after mitigation
+$SpeculationControlResultsAfter = Get-SpeculationControlSettings
+$SpeculationControlResultsAfter
+#endregion Get-SpeculationControlSettings Results after mitigation
+
+
+#region Reset the execution policy to the original state
 Set-ExecutionPolicy $SavedExecutionPolicy -Scope Currentuser
-# Stop Logging
+#endregion Reset the execution policy to the original state
+
+#region Stop Logging
 Stop-Transcript
+#endregion Stop Logging
