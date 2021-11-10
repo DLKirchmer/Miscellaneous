4bcd520f45cd18d60322a4941f0928a11c25d70f
David Kirchmer <david.KMSA@gmail.com>
2021-11-10 15:13:27 -0500

    Refactored to add $DotNetVersion variable

--- Get-DotNetVersion.ps1
+++ Get-DotNetVersion.ps1
@@ -1,3 +1,5 @@
 #Get-DotNetVersion.ps1
 
-Get-ChildItem 'HKLM:\SOFTWARE\Microsoft\NET Framework Setup\NDP' -Recurse | Get-ItemProperty -Name version -EA 0 | Where-Object { $_.PSChildName -Match '^(?!S)\p{L}'} | Select-Object PSChildName, version
+$DotNetVersion = Get-ChildItem 'HKLM:\SOFTWARE\Microsoft\NET Framework Setup\NDP' -Recurse | Get-ItemProperty -Name version -EA 0 | Where-Object { $_.PSChildName -Match '^(?!S)\p{L}'}  | Select-Object PSChildName, version 
+
+$DotNetVersion
