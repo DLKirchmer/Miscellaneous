eae2db5f2eba2d28a9e0d21c24635c2e756a17f7
David Kirchmer <david.KMSA@gmail.com>
2021-11-12 23:35:11 -0500

    Returns DotNetVersion.Version

--- Get-DotNetVersion.ps1
+++ Get-DotNetVersion.ps1
@@ -2,4 +2,6 @@
 
 $DotNetVersion = Get-ChildItem 'HKLM:\SOFTWARE\Microsoft\NET Framework Setup\NDP' -Recurse | Get-ItemProperty -Name version -EA 0 | Where-Object { $_.PSChildName -Match '^(?!S)\p{L}'}  | Select-Object PSChildName, version 
 
+$DotNetVersion.Count
+
 $DotNetVersion
