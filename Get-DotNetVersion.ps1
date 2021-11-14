bbcd4a889d1ff9c8ec880233e0659e95a154947e
David Kirchmer <david.KMSA@gmail.com>
2021-11-13 20:34:42 -0500

    Added Write-output to return Version as a string

--- Get-DotNetVersion.ps1
+++ Get-DotNetVersion.ps1
@@ -1,7 +1,11 @@
 #Get-DotNetVersion.ps1
 
-$DotNetVersion = Get-ChildItem 'HKLM:\SOFTWARE\Microsoft\NET Framework Setup\NDP' -Recurse | Get-ItemProperty -Name version -EA 0 | Where-Object { $_.PSChildName -Match '^(?!S)\p{L}'}  | Select-Object PSChildName, version 
+$DotNetVersions = Get-ChildItem 'HKLM:\SOFTWARE\Microsoft\NET Framework Setup\NDP' -Recurse | Get-ItemProperty -Name version -EA 0 | Where-Object { $_.PSChildName -Match '^(?!S)\p{L}'}  | Select-Object PSChildName, version 
 
-$DotNetVersion.Count
+#$DotNetVersion.Count
 
-$DotNetVersion
+$DotNetVersions
+
+ForEach ($DotNetVersion in $DotNetVersions) {
+    Write-Output $DotNetVersion.version
+}
